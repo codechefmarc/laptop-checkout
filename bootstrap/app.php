07b0aba8332debf5f997890fda6278176b87ac84
Marc Berger <codechefmarc@gmail.com>
2025-09-02 10:57:39 -0700

    feat: Start to add Student role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\CanEditMiddleware;
+use App\Http\Middleware\IsStudentMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
       $middleware->alias([
         'admin' => AdminMiddleware::class,
         'can.edit' => CanEditMiddleware::class,
+        'is.student' => IsStudentMiddleware::class,
       ]);
   })
   ->withExceptions(function (Exceptions $exceptions): void {
